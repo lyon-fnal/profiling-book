a8c1f2bd9ebfd988f5b994b73403a9b49dbed8c7
Adam Lyon <lyon@fnal.gov>
2017-05-02 14:17:49 -0500

    Add comment to _render.R

--- _render.R
+++ _render.R
@@ -1,3 +1,4 @@
+# source this from within RStudio - writes to docs for uploading to github.io
 system('mkdir -p docs/images ; cp images/* docs/images/')
 bookdown::render_book('index.Rmd','bookdown::gitbook', clean=FALSE )
 
